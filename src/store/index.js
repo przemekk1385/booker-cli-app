f98e2fc763498fae1d83967f5d202166460b521a
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-13 23:12:56 +0100

    integrate with API

--- src/store/index.js
+++ src/store/index.js
@@ -1,78 +1,66 @@
 import { createStore } from "vuex";
+import axios from "axios";
+import dayjs from "dayjs";
 
 export default createStore({
   state: {
-    today: "2021-12-06",
-    bookings: [
-      {
-        apartment: "Apartment 1",
-        day: "2021-12-06",
-        slotLabel: "11:00 - 11:50",
-        slot: 1,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "12:00 - 12:50",
-        slot: 2,
-      },
-      {
-        apartment: "Apartment 2",
-        day: "2021-12-06",
-        slotLabel: "13:00 - 13:50",
-        slot: 3,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "14:00 - 14:50",
-        slot: 4,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "15:00 - 15:50",
-        slot: 5,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "16:00 - 16:50",
-        slot: 6,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "17:00 - 17:50",
-        slot: 7,
-      },
-      {
-        apartment: "Apartment 7",
-        day: "2021-12-06",
-        slotLabel: "18:00 - 18:50",
-        slot: 8,
-      },
-      {
-        apartment: null,
-        day: "2021-12-06",
-        slotLabel: "19:00 - 19:50",
-        slot: 9,
-      },
-      {
-        apartment: "Apartment 8",
-        day: "2021-12-06",
-        slotLabel: "20:00 - 20:50",
-        slot: 10,
-      },
-      {
-        apartment: "Apartment 9",
-        day: "2021-12-06",
-        slotLabel: "21:00 - 21:50",
-        slot: 11,
-      },
-    ],
+    errorMessage: undefined,
+    minDate: dayjs(),
+    maxDate: dayjs().add(1, "day"),
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    errorMessage(state, message) {
+      state.errorMessage = message;
+    },
+  },
+  actions: {
+    async slotList({ commit }) {
+      try {
+        const dataPromise = await axios.get(
+          `${process.env.VUE_APP_API_HOST}/api/v1/slot/`
+        );
+        const { data } = dataPromise;
+        return data;
+      } catch ({ response: { data, status } }) {
+        commit(
+          "errorMessage",
+          `Failed to get slots, got ${status} response code.`
+        );
+        return [];
+      }
+    },
+    async bookingCreate({ commit }, { day, identifier, slot }) {
+      try {
+        const dataPromise = await axios.post(
+          `${process.env.VUE_APP_API_HOST}/api/v1/booking/`,
+          {
+            day,
+            identifier,
+            slot,
+          }
+        );
+        const { data } = dataPromise;
+        return data;
+      } catch ({ response: { data, status } }) {
+        commit("errorMessage", `Failed to book, got ${status} response code.`);
+        return { errors: data };
+      }
+    },
+    async bookingList({ commit }) {
+      try {
+        const dataPromise = await axios.get(
+          `${process.env.VUE_APP_API_HOST}/api/v1/booking/`
+        );
+        const { data } = dataPromise;
+        return data;
+      } catch ({ response: { data, status } }) {
+        commit(
+          "errorMessage",
+          `Failed to get bookings, got ${status} response code.`
+        );
+        return [];
+      }
+    },
+  },
   modules: {},
 });
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -4,8 +4,8 @@
       <div class="p-fluid">
         <form
           class="p-fluid"
-          @reset="resetForm()"
-          @submit.prevent="handleSubmit(!v$.$invalid)"
+          @reset="handleReset()"
+          @submit.prevent="handleSubmit()"
         >
           <div class="p-field">
             <span class="p-input-icon-right">
@@ -13,50 +13,55 @@
               <InputMask
                 id="identifier"
                 v-model="v$.identifier.$model"
-                :class="{ 'p-invalid': v$.identifier.$invalid && submitted }"
+                :autoClear="false"
+                :class="{
+                  'p-invalid': v$.identifier.$invalid && v$.identifier.$dirty,
+                }"
                 mask="999-999-999"
+                :modelValue="v$.identifier.$model"
                 type="text"
               />
             </span>
             <small
-              v-if="
-                (v$.identifier.$invalid && submitted) ||
-                v$.identifier.$pending.$response
-              "
-              class="p-error"
-              >{{
-                v$.identifier.required.$message.replace("Value", "Phone number")
-              }}</small
+              v-for="error of v$.identifier.$errors"
+              :key="error.$uid"
+              class="p-d-block p-error"
             >
+              {{ error.$message }}
+            </small>
           </div>
           <div class="p-field">
             <Calendar
               inputId="calendar"
               v-model="v$.day.$model"
-              :class="{ 'p-invalid': v$.day.$invalid && submitted }"
+              :class="{ 'p-invalid': v$.day.$invalid && v$.day.$dirty }"
               dateFormat="yy-mm-dd"
+              :minDate="calendar.minDate"
+              :maxDate="calendar.maxDate"
               :showIcon="true"
             />
             <small
-              v-if="(v$.day.$invalid && submitted) || v$.day.$pending.$response"
-              class="p-error"
-              >{{ v$.day.required.$message.replace("Value", "Day") }}</small
+              v-for="error of v$.day.$errors"
+              :key="error.$uid"
+              class="p-d-block p-error"
             >
+              {{ error.$message }}
+            </small>
           </div>
           <div class="p-field">
             <Dropdown
               inputId="slot"
               v-model="v$.slot.$model"
-              :class="{ 'p-invalid': v$.slot.$invalid && submitted }"
+              :class="{ 'p-invalid': v$.slot.$invalid && v$.slot.$dirty }"
               optionDisabled="apartment"
-              optionLabel="slotLabel"
-              optionValue="slot"
-              :options="bookings"
+              optionLabel="label"
+              optionValue="value"
+              :options="slots"
               placeholder="Choose slot"
             >
               <template #option="slotProps">
                 <div class="p-ai-center p-jc-between p-d-flex">
-                  {{ slotProps.option.slotLabel }}
+                  {{ slotProps.option.label }}
                   <Tag
                     v-if="slotProps.option.apartment"
                     :value="slotProps.option.apartment"
@@ -65,13 +70,14 @@
               </template>
             </Dropdown>
             <small
-              v-if="
-                (v$.slot.$invalid && submitted) || v$.slot.$pending.$response
-              "
-              class="p-error"
-              >{{ v$.slot.required.$message.replace("Value", "Slot") }}</small
+              v-for="error of v$.slot.$errors"
+              :key="error.$uid"
+              class="p-d-block p-error"
             >
+              {{ error.$message }}
+            </small>
           </div>
+
           <span class="p-buttonset">
             <Button icon="pi pi-check" type="submit" />
             <Button class="p-button-danger" icon="pi pi-times" type="reset" />
@@ -87,51 +93,91 @@ import { onMounted, reactive, ref } from "vue";
 import { required } from "@vuelidate/validators";
 import { useStore } from "vuex";
 import { useVuelidate } from "@vuelidate/core";
+import dayjs from "dayjs";
 
 export default {
   setup() {
     const store = useStore();
 
     const rules = {
-      identifier: { required },
       day: { required },
+      identifier: { required },
       slot: { required },
     };
-
     const state = reactive({
-      identifier: "",
-      day: "",
-      slot: "",
+      day: undefined,
+      identifier: undefined,
+      slot: undefined,
+    });
+    const $externalResults = reactive({});
+
+    const v$ = useVuelidate(rules, state, {
+      $externalResults,
     });
 
-    const v$ = useVuelidate(rules, state);
+    const handleSubmit = async () => {
+      v$.value.$clearExternalResults();
 
-    const submitted = ref(false);
-    let bookings = ref([]);
+      if (!(await v$.value.$validate())) return;
 
-    const handleSubmit = (isFormValid) => {
-      submitted.value = true;
+      let { day } = state;
+      const { identifier, slot } = state;
+      const data = await store.dispatch("bookingCreate", {
+        day: dayjs(day).format("YYYY-MM-DD"),
+        identifier,
+        slot,
+      });
 
-      if (!isFormValid) {
-        return;
+      const { errors } = data;
+      if (errors) {
+        Object.assign($externalResults, errors);
       }
     };
-    const resetForm = () => {
-      state.identifier = "";
-      state.day = "";
-      state.slot = "";
-      submitted.value = false;
+    const handleReset = () => {
+      state.day = undefined;
+      state.identifier = undefined;
+      state.slot = undefined;
+      v$.value.$reset();
     };
 
-    const getTodaysDate = () => store.state.today;
-    const getBookings = () => store.state.bookings;
-
-    onMounted(() => {
-      state.day = getTodaysDate();
-      bookings.value = getBookings();
+    const calendar = reactive({
+      maxDate: undefined,
+      minDate: undefined,
     });
 
-    return { state, v$, submitted, bookings, handleSubmit, resetForm };
+    const slots = ref([]);
+
+    onMounted(async () => {
+      const maxDate = (() => store.state.maxDate)();
+      const minDate = (() => store.state.minDate)();
+
+      state.day = minDate.toDate();
+      calendar.maxDate = maxDate.toDate();
+      calendar.minDate = minDate.toDate();
+
+      const bookingListPromise = await (() => store.dispatch("bookingList"))();
+      const bookings = bookingListPromise.reduce(
+        (ac, { slot, apartment }) => ({ ...ac, [slot]: apartment }),
+        {}
+      );
+
+      const slotListPromise = await (() => store.dispatch("slotList"))();
+
+      slots.value = slotListPromise.map(({ label, value }) => ({
+        label,
+        value,
+        apartment: bookings[value],
+      }));
+    });
+
+    return {
+      state,
+      v$,
+      handleSubmit,
+      handleReset,
+      calendar,
+      slots,
+    };
   },
 };
 </script>
