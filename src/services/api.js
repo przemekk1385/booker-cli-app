1ecbae745363bd0989971b5734a0fd9deb6d95b0
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-17 00:44:27 +0100

    move db and api calls to external modules

---
+++ src/services/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const bookingList = async () =>
+  await axios
+    .get(`${process.env.VUE_APP_API_HOST}/api/v1/booking/`)
+    .then(({ data, status }) => ({ data, status }))
+    .catch(({ response: { status } = {} }) => ({ status }));
+
+export const bookingCreate = async ({ day, identifier, slot }) =>
+  await axios
+    .post(`${process.env.VUE_APP_API_HOST}/api/v1/booking/`, {
+      day,
+      identifier,
+      slot,
+    })
+    .then(({ data, status }) => ({ data, status }))
+    .catch(({ response: { data: errors, status } = {} }) => {
+      if (status === 400) {
+        return { errors, status };
+      } else {
+        return {
+          status,
+        };
+      }
+    });
+
+export const slotList = async () =>
+  await axios
+    .get(`${process.env.VUE_APP_API_HOST}/api/v1/slot/`)
+    .then(({ data, status }) => ({ data, status }))
+    .catch(({ response: { status = undefined } = {} }) => ({ status }));
--- src/store/index.js
+++ src/store/index.js
@@ -3,39 +3,14 @@ import { createStore } from "vuex";
 import axios from "axios";
 import dayjs from "dayjs";
 
+import { getDatabase, slotList as storeSlotList } from "@/services/database";
+import {
+  bookingList,
+  bookingCreate,
+  slotList as apiSlotList,
+} from "@/services/api";
+
 const today = dayjs();
-let database = null; // TODO: move outside
-
-const getDatabase = () => {
-  return new Promise((resolve, reject) => {
-    if (database) {
-      resolve(database);
-    }
-
-    const request = window.indexedDB.open("bookerClientDB");
-
-    request.onerror = (event) => {
-      console.log("ERROR: Unable to open database", event);
-      reject("Error");
-    };
-
-    request.onsuccess = (event) => {
-      database = event.target.result;
-      resolve(database);
-    };
-
-    request.onupgradeneeded = (event) => {
-      database = event.target.result;
-
-      database.createObjectStore("slots", {
-        keyPath: "value",
-      });
-      database.createObjectStore("bookings", {
-        keyPath: "id",
-      });
-    };
-  });
-};
 
 const state = {
   day: today.toDate(),
@@ -54,7 +29,7 @@ const state = {
 
 const getters = {
   isApiOnline: ({ healthStatus }) => healthStatus === 200,
-  isAppSyncing: ({ bookings, slots }) => !bookings || !slots,
+  isAppSyncing: ({ bookings, slots }) => !bookings || !slots, // TODO: fix
   latestFormErrors: ({ formErrors }) => formErrors[formErrors.length - 1],
   latestMessage: ({ messages }) => messages[messages.length - 1],
 
@@ -111,62 +86,34 @@ const actions = {
     await dispatch("fetchBookingsFromApi");
   },
   async fetchBookingsFromApi({ commit }) {
-    const bookings = await axios
-      .get(`${process.env.VUE_APP_API_HOST}/api/v1/booking/`)
-      .then(({ data: bookings }) => bookings)
-      .catch(({ response: { status } = {} }) => {
-        if (status) {
-          commit("pushMessage", {
-            severity: ToastSeverity.ERROR,
-            summary: "Error",
-            detail: `Failed to get bookings, got ${status} response code.`,
-          });
-        }
-        return [];
+    const { data: bookings, status } = await bookingList();
+
+    if (!status) {
+      commit("pushMessage", {
+        severity: ToastSeverity.ERROR,
+        summary: "Error",
+        detail: `Failed to get bookings, got ${status} response code.`,
       });
-    commit("bookings", bookings);
+    } else {
+      commit("bookings", bookings);
+    }
   },
   async fetchSlotsFromApi({ commit }) {
-    const slots = await axios
-      .get(`${process.env.VUE_APP_API_HOST}/api/v1/slot/`)
-      .then(({ data: slots }) => slots)
-      .catch((error) => {
-        const { response: { status } = {} } = error;
-        if (status) {
-          commit("pushMessage", {
-            severity: ToastSeverity.ERROR,
-            summary: "Error",
-            detail: `Failed to get slots, got ${status} response code.`,
-          });
-        }
-        return [];
+    const { data: slots, status } = await apiSlotList();
+
+    if (!status) {
+      commit("pushMessage", {
+        severity: ToastSeverity.ERROR,
+        summary: "Error",
+        detail: `Failed to get slots, got ${status} response code.`,
       });
-    commit("slots", slots);
+    } else {
+      commit("slots", slots);
+    }
   },
   async fetchSlotsFromStore({ commit }) {
-    const database = await getDatabase();
+    const slots = await storeSlotList(); // TODO: error handling (?)
 
-    const slots = await new Promise((resolve, reject) => {
-      const transaction = database.transaction("slots", "readonly");
-      const store = transaction.objectStore("slots");
-      let slots = [];
-
-      store.openCursor().onsuccess = (event) => {
-        let cursor = event.target.result;
-        if (cursor) {
-          slots.push(cursor.value);
-          cursor.continue();
-        }
-      };
-
-      transaction.oncomplete = () => {
-        resolve(slots);
-      };
-
-      transaction.onerror = (event) => {
-        reject(event);
-      };
-    });
     commit("slots", slots);
   },
   async fetchHealthStatus({ commit }) {
@@ -179,39 +126,32 @@ const actions = {
     );
   },
 
-  async createBooking({ commit }, { day, identifier, slot }) {
-    await axios
-      .post(`${process.env.VUE_APP_API_HOST}/api/v1/booking/`, {
-        day,
-        identifier,
-        slot,
-      })
-      .then(({ data: booking }) => {
-        commit("pushBooking", booking);
-        commit("pushMessage", {
-          severity: ToastSeverity.SUCCESS,
-          summary: "Ok",
-          detail: `Booking created.`,
-          life: 3000,
-        });
-      })
-      .catch(({ response: { data: errors, status } = {} }) => {
-        if (status === 400) {
-          commit("pushFormErrors", errors);
-          commit("pushMessage", {
-            severity: ToastSeverity.INFO,
-            summary: "Info",
-            detail: `Check form fields.`,
-            life: 3000,
-          });
-        } else if (status) {
-          commit("pushMessage", {
-            severity: ToastSeverity.ERROR,
-            summary: "Error",
-            detail: `Failed to book, got ${status} response code.`,
-          });
-        }
+  async createBooking({ commit }, payload) {
+    const { data: booking, errors, status } = await bookingCreate(payload);
+
+    if (status === 201) {
+      commit("pushBooking", booking);
+      commit("pushMessage", {
+        severity: ToastSeverity.SUCCESS,
+        summary: "Ok",
+        detail: `Booking created.`,
+        life: 3000,
       });
+    } else if (status === 400) {
+      commit("pushFormErrors", errors);
+      commit("pushMessage", {
+        severity: ToastSeverity.INFO,
+        summary: "Info",
+        detail: `Check form fields.`,
+        life: 3000,
+      });
+    } else if (status) {
+      commit("pushMessage", {
+        severity: ToastSeverity.ERROR,
+        summary: "Error",
+        detail: `Failed to book, got ${status} response code.`,
+      });
+    }
   },
 };
 
